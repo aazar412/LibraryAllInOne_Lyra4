c16a4bb359a2262f7ffbebc6a97e4240ba790759
mehmetCt <mehmet@cybertekschool.com>
2023-04-12 13:35:23 -0400

    US 01

--- src/test/java/com/library/steps/APIStepDefs.java
+++ src/test/java/com/library/steps/APIStepDefs.java
@@ -1,11 +1,22 @@
 package com.library.steps;
 
+import com.library.utility.ConfigurationReader;
+import com.library.utility.LibraryAPI_Util;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+import static io.restassured.RestAssured.*;
 
 public class APIStepDefs {
 
+    RequestSpecification givenPart;
+    Response response;
+    ValidatableResponse thenPart;
     /**
      * US 01 RELATED STEPS
      *
@@ -13,14 +24,17 @@ public class APIStepDefs {
     @Given("I logged Library api as a {string}")
     public void i_logged_library_api_as_a(String userType) {
 
+        givenPart = given().log().uri()
+                .header("x-library-token", LibraryAPI_Util.getToken(userType));
     }
     @Given("Accept header is {string}")
     public void accept_header_is(String contentType) {
-
+        givenPart.accept(contentType);
     }
     @When("I send GET request to {string} endpoint")
     public void i_send_get_request_to_endpoint(String endpoint) {
-
+        response = givenPart.when().get(ConfigurationReader.getProperty("library.baseUri") + endpoint).prettyPeek();
+        thenPart = response.then();
     }
     @Then("status code should be {int}")
     public void status_code_should_be(Integer statusCode) {
