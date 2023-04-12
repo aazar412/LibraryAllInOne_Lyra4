6a64bfd423120f30f1788f1c1c27fee1488b7ddc
mehmetCt <mehmet@cybertekschool.com>
2023-04-12 13:45:55 -0400

    US 01

--- src/test/java/com/library/steps/APIStepDefs.java
+++ src/test/java/com/library/steps/APIStepDefs.java
@@ -6,11 +6,14 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matchers;
 
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
 
 public class APIStepDefs {
 
@@ -31,6 +34,7 @@ public class APIStepDefs {
     public void accept_header_is(String contentType) {
         givenPart.accept(contentType);
     }
+
     @When("I send GET request to {string} endpoint")
     public void i_send_get_request_to_endpoint(String endpoint) {
         response = givenPart.when().get(ConfigurationReader.getProperty("library.baseUri") + endpoint).prettyPeek();
@@ -38,15 +42,15 @@ public class APIStepDefs {
     }
     @Then("status code should be {int}")
     public void status_code_should_be(Integer statusCode) {
-
+        thenPart.statusCode(statusCode);
     }
     @Then("Response Content type is {string}")
     public void response_content_type_is(String contentType) {
-
+        thenPart.contentType(contentType);
     }
     @Then("{string} field should not be null")
     public void field_should_not_be_null(String path) {
-
+        thenPart.body(path, everyItem(notNullValue()));
     }
 
 }
